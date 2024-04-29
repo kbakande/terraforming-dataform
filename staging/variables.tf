5ca6eb05139cc006a68ae10e70fc474dfd10b25d
Kabeer Akande <46060542+kbakande@users.noreply.github.com>
2024-04-29 19:07:18 +0100

    adds variables

---
+++ staging/variables.tf
@@ -0,0 +1,42 @@
+variable "project_id" {
+  type        = string
+  description = "Name of the GCP Project."
+}
+
+variable "env" {
+  type        = string
+  description = "Environment Tag (nonprod/prod)."
+}
+
+variable "region" {
+  type        = string
+  description = "The google cloud region to use"
+  default     = "europe-west2"
+}
+
+variable "project_number" {
+  type        = string
+  description = "Number of the GCP Project."
+}
+
+variable "location" {
+  type        = string
+  description = "The google cloud location in which to create resources"
+  default     = "EU"
+}
+
+variable "dataform_nonprod_service_account" {
+  type        = string
+  description = "Email of the service account Dataform uses to execute queries in nonprod"
+}
+
+variable "dataform_prod_service_account" {
+  type        = string
+  description = "Email of the service account Dataform uses to execute queries in production"
+}
+
+variable "dataform_github_token" {
+  description = "Dataform GitHub Token"
+  type        = string
+  sensitive   = true
+}
