5aba380fd2f7023c6e8cf0822f3be81b129808f3
Kabeer Akande <46060542+kbakande@users.noreply.github.com>
2024-05-29 11:17:40 +0100

    adds least permission to default service account

--- staging/iam.tf
+++ staging/iam.tf
@@ -15,37 +15,30 @@ resource "google_project_iam_member" "dataform_staging_roles" {
   ]
 }
 
-resource "google_project_iam_member" "default_dataform_sa_staging_role" {
-  project = var.project_id
+resource "google_service_account_iam_binding" "custom_service_account_token_creator" {
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.dataform_staging_service_account}@${var.project_id}.iam.gserviceaccount.com"
 
-  role   = "roles/iam.serviceAccountTokenCreator"
-  member = "serviceAccount:service-${var.project_number}@gcp-sa-dataform.iam.gserviceaccount.com"
+  role = "roles/iam.serviceAccountTokenCreator"
 
-  condition {
-    title       = "RestrictToSpecificServiceAccount"
-    description = "Applies this role only if the token is requested for the specific custom service account."
-    expression  = "resource.name.startsWith('projects/${var.project_number}/serviceAccounts/${var.dataform_staging_service_account}@${var.project_id}.iam.gserviceaccount.com')"
-  }
-
+  members = [
+    "serviceAccount:service-${var.project_number}@gcp-sa-dataform.iam.gserviceaccount.com"
+  ]
   depends_on = [
-    google_service_account.dataform_staging
+    module.service-accounts
   ]
 }
 
-resource "google_project_iam_member" "default_dataform_sa_secret_assess" {
-  project = var.project_id
+resource "google_secret_manager_secret_iam_binding" "github_secret_accessor" {
+  secret_id = google_secret_manager_secret.dataform_github_token.secret_id
 
-  role   = "roles/secretmanager.secretAccessor"
-  member = "serviceAccount:service-${var.project_number}@gcp-sa-dataform.iam.gserviceaccount.com"
+  role = "roles/secretmanager.secretAccessor"
 
-  condition {
-    title       = "RestrictToSpecificSecret"
-    description = "Applies this role only if accessing staging dataform github token."
-    expression  = "resource.name.startsWith('projects/${var.project_number}/secrets/${google_secret_manager_secret.dataform_github_token.secret_id}')"
-  }
+  members = [
+    "serviceAccount:service-${var.project_number}@gcp-sa-dataform.iam.gserviceaccount.com"
+  ]
 
   depends_on = [
     google_secret_manager_secret.dataform_github_token,
-    google_service_account.dataform_staging
+    module.service-accounts,
   ]
 }
